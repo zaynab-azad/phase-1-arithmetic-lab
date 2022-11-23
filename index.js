5d8cb3cce1162c9738c8071411cfc774152b435c
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-11-23 20:36:29 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+let random = Number.parseInt(Math.random() + 1);
 
+let num3 = 13;
+let num4 = 9;
+let mod = num3 % num4;
+let max = Math.max(0, 15, 12, 20, 18);
 // Write your code here
